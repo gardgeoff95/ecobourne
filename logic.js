f1422721378f2ab81cbca9032ac6ed43f101ee49
Geoff <gardgeoff95@email.arizona.edu>
2019-09-21 16:55:19 -0700

    click to add wall, wall collision

--- logic.js
+++ logic.js
@@ -1,6 +1,4 @@
-
 let range = document.getElementById("range");
-
 const board = {
   maxTiles: 50,
   tileWidth: 16,
@@ -11,13 +9,17 @@ const ctx = canvas.getContext("2d");
 const backgroundCanvas = document.getElementById("background");
 const backGroundCtx = backgroundCanvas.getContext("2d");
 
+let mapArray = new Array(50);
+let bunniesArray = [];
+
 backgroundCanvas.width = 800;
 backgroundCanvas.height = 800;
 canvas.height = board.tileHeight * board.maxTiles;
 canvas.width = board.tileWidth * board.maxTiles;
 
-let mapArray = new Array(50);
-
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 function drawBackground(x, y, color) {
   backGroundCtx.fillStyle = color;
   backGroundCtx.fillRect(
@@ -41,9 +43,15 @@ function createArray() {
   for (let i = 0; i < board.maxTiles; i++) {
     mapArray[i] = [];
   }
-  for (let i = 0; i < board.maxTiles; i++) {
-    for (let j = 0; j < board.maxTiles; j++) {
-      mapArray[i][j] = 0;
+  for (let x = 0; x < board.maxTiles; x++) {
+    for (let y = 0; y < board.maxTiles; y++) {
+      if (randomNumber(1, 100) == 5){
+        mapArray[x][y] = 1;
+
+      } else {
+        mapArray[x][y] = 0;
+      }
+      
     }
   }
   renderBackground();
@@ -52,23 +60,23 @@ function createArray() {
 function renderBackground() {
   for (let y = 0; y < mapArray.length; y++) {
     for (let x = 0; x < mapArray.length; x++) {
-      switch (mapArray[x][y]) {
+      switch (mapArray[y][x]) {
         case 0:
           drawBackground(x, y, "#567d46");
           break;
-        default:
-          ctx.fillStyle = "black";
-
-          backGroundCtx.fillRect(0, 0, 50, 50);
+        case 1:
+          drawBackground(x, y, "black");
+          break;
       }
     }
   }
-  window.requestAnimationFrame(mainLoop);
+
 }
-let Animal = function(vitality,  color, speedModifier) {
+
+let Animal = function(vitality, color, speedModifier) {
   this.col = 20;
   this.row = 20;
-  this.speedModifier =speedModifier
+  this.speedModifier = speedModifier;
   this.color = color;
   this.moveDelay = (range.value / 2) * this.speedModifier;
   this.moveCounter = 0;
@@ -76,31 +84,35 @@ let Animal = function(vitality,  color, speedModifier) {
   this.alive = true;
   this.updateDelay = function() {
     this.moveDelay = range.value / 2;
-  }
+  };
   this.draw = function() {
     drawAnimals(this.col, this.row, this.color);
   };
   this.idle = function() {
     let possibleJumps = [];
-    if (mapArray[this.row + 2][this.col] == 0) {
-      possibleJumps.push("up");
-    }
-    if (mapArray[this.row - 2][this.col] == 0) {
+    let direction;
+    
+    
+
+    if (this.row + 1 < 50 && mapArray[this.row + 1][this.col] === 0) {
       possibleJumps.push("down");
     }
-    if (mapArray[this.row][this.col + 2] == 0) {
+    if (this.row - 1 > 0 && mapArray[this.row - 1][this.col] === 0) {
+      possibleJumps.push("up");
+    }
+    if (mapArray[this.row][this.col + 1] === 0) {
       possibleJumps.push("right");
     }
-    if (mapArray[this.row][this.col - 2] == 0) {
+    if (mapArray[this.row][this.col - 1] === 0 ) {
       possibleJumps.push("left");
     }
+  
 
-    let direction = possibleJumps[Math.floor(Math.random() * possibleJumps.length)];
-   
+    direction =
+      possibleJumps[Math.floor(Math.random() * possibleJumps.length)];
     if (this.alive) {
-        this.moveCounter += this.moveDelay
+      this.moveCounter += this.moveDelay;
       if (this.moveCounter > 100) {
-       
         switch (direction) {
           case "up":
             this.row -= 1;
@@ -112,23 +124,16 @@ let Animal = function(vitality,  color, speedModifier) {
             this.col -= 1;
             break;
           case "right":
-            this.col +=1;
-            
+            this.col += 1;
         }
         this.moveCounter = 0;
+
+        
+
       }
     }
   };
 
-  this.moveDown = function() {
-    if (this.alive) {
-      this.moveCounter += this.moveDelay;
-      if (this.moveCounter > 100) {
-        this.row += 1;
-        this.moveCounter = 0;
-      }
-    }
-  };
   this.die = function() {
     this.timeAlive++;
     if (this.timeAlive >= vitality) {
@@ -137,22 +142,54 @@ let Animal = function(vitality,  color, speedModifier) {
     }
   };
 };
+function click(event) {
+  var x = event.clientX;
+  var y = event.clientY;
+  console.log(x + " " + y);
+}
+canvas.addEventListener(
+  "click",
+  function(evt) {
+    var mousePos = getMousePos(canvas, evt);
+    
+    mapArray[mousePos.y][mousePos.x] = 1;
+    renderBackground();
+  },
+  false
+);
 
-bunniesArray = [];
-let bunny = new Animal(100, "yellow", 20);
-let newBunny = new Animal(400, "blue", 20);
+//Get Mouse Position
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: Math.floor((evt.clientX - rect.left) / board.tileWidth),
+    y: Math.floor((evt.clientY - rect.top) / board.tileHeight)
+  };
+}
+
+let bunny = new Animal(
+  100,
+  "yellow",
+  20
+);
+let bunny2 = new Animal(
+  100,
+  "yellow",
+  20
+);
 
 createArray();
+console.log(mapArray);
 let frameCount = 0;
 function mainLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   bunny.draw();
   bunny.idle();
+  bunny.die();
+
   bunny.updateDelay();
-  newBunny.draw();
-  newBunny.idle();
-  newBunny.updateDelay();
-  
+
 
 }
 setInterval(mainLoop, 10);
--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Create the browser window.
   let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       nodeIntegration: true
     }
