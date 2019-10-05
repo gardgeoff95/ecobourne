8a7286daaecbf357e2f17a4ef0dd783474934cf1
aznchronos <aznchronos@users.noreply.github.com>
2019-10-05 10:12:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mongodb = require("mongodb");
+const io = require("socket.io");
 const routes = require("./routes");
 
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Parse request body as JSON
@@ -24,6 +26,33 @@ mongoose.connect(
     
 app.use(routes);
 
-app.listen(PORT, () =>
-    console.log(`API Server now listening to PORT ${PORT}`)
-);
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
+
+///////////////////////////////////////////
+//Socket Stuff
+///////////////////////////////////////////
+
+var users = [];
+//Connects
+io.on("connection", function(socket) {
+  console.log("a user connected");
+
+  //Sends a chat message
+  socket.on("chat message", function(msg) {
+    console.log("My Chat Message listener", msg);
+
+    io.emit("chat message", msg);
+  });
+  //Deals with user name
+  socket.on("send-nickname", function(nickname) {
+    socket.nickname = nickname;
+    users.push(socket.nickname);
+    console.log(users);
+  });
+  //Shows that user disconnects
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+});
