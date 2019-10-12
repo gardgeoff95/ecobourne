11d2de1457ecf7c0c072f199f5eb84c67d31256b
Jamie Barton <jbarton10@email.arizona.edu>
2019-10-12 10:38:27 -0700

    saved git ignore

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,10 +1,44 @@
 import React, { Component } from "react";
+import axios from "axios";
 // import LoginForm from '../../components/login/login';
 
 import mobiscroll from "@mobiscroll/react";
 import "@mobiscroll/react/dist/css/mobiscroll.min.css";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  onChangeEmail = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  onChangePassword = event => {
+    this.setState({
+      password: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    //More name verification should go on here.
+    if (!(this.state.email === "") && !(this.state.password === "")) {
+      axios
+        .post("/", { logEmail: this.state.email, pword: this.state.password })
+        .then(res => {
+          if (res.data.correct) {
+            this.props.goToTitleScreen;
+          } else {
+            console.log("WRONG");
+          }
+        });
+    }
+  };
+
   render() {
     return (
       <div className="container-fluid">
@@ -14,10 +48,20 @@ class Login extends Component {
               <label>
                 <input
                   className="fadeIn"
-                  placeholder="Nickname"
+                  placeholder="Email"
                   type="text"
-                  value={this.state.value}
-                  onChange={this.onChange}
+                  value={this.state.email}
+                  onChange={this.onChangeEmail}
+                />
+              </label>
+
+              <label>
+                <input
+                  className="fadeIn"
+                  placeholder="Password"
+                  type="text"
+                  value={this.state.password}
+                  onChange={this.onChangePassword}
                 />
               </label>
 
